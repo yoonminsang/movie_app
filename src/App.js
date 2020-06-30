07912b8474daccba8f796fc14ab0c3fa3fad0d9f
yoonminsang <apdlyooapdl@naver.com>
2020-06-30 15:09:15 +0900

    first day

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import axios from "axios";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
+}
+
+export default App;
